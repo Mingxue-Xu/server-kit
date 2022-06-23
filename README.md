18e3234baac88a8fc11cee1d1ebf2dbbed39aca0
Mingxue-Xu <92848346+Mingxue-Xu@users.noreply.github.com>
2022-06-23 19:29:33 +0100

    network connection

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# server-kit
+# Server-kit
+A tiny kit that contains some common functions for GPU/CPU central/edge servers.
+Keep updating:D
+
+## Network Check and Reconnection
+A simple bash shell script periodically checks the network connection (static IP), reconnects, and generates logs.
+```shell
+sudo sh network/network-check.sh ip
+```
---
+++ network/network-check.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/sh
+LOG_PATH="logs/network_check.txt"
+IP_ADDR=$1
+INTERVAL=10
+THED=0
+while true
+do
+    sleep $INTERVAL
+    is_working=$(ifconfig | grep $IP_ADDR)
+    size=${#is_working}
+    currentTime=`date "+%Y-%m-%d %H:%M:%S"`
+    if [ $size -eq $THED ]
+    then
+        cards=$(lshw -class network | grep logical |  awk '{print $3}')
+        for c in $cards
+            do
+                ifconfig "$c" down
+                ifconfig "$c" up
+                echo "$currentTime restart $c" >> $LOG_PATH
+            done
+    else
+        echo "$currentTime is working" >> $LOG_PATH
+    fi
+done
